5934985fc95ad2c7f4bb4b00c4fbd618f9d36603
burialstance <burialstance@gmail.com>
2021-11-02 23:12:08 +0500

    UserMiddleware now only get <User> without create

--- middlewares/userdata.py
+++ middlewares/userdata.py
@@ -1,4 +1,5 @@
-from aiogram.dispatcher.handler import current_handler
+import loguru
+from aiogram.dispatcher.handler import current_handler, CancelHandler
 from aiogram.dispatcher.middlewares import BaseMiddleware
 from aiogram import types
 
@@ -16,15 +17,21 @@ class UserMiddleware(BaseMiddleware):
     def __init__(self):
         super(UserMiddleware, self).__init__()
 
-    async def push_user_to_context(self, user_id: int, context: dict):
+    async def push_user_to_context(self, user_id: int, data: dict):
         handler = current_handler.get()
         if handler and getattr(handler, 'userdata_required', False):
-            telegram_user = types.User.get_current()
-            user, _ = await User.get_or_create(**telegram_user.to_python())
-            context['user'] = user
+            user = await User.get_or_none(id=user_id)
+            if user:
+                data['user'] = user
+                return
+
+            await self.manager.bot.send_message(chat_id=user_id, text='Для этого действия Требуется регистрация /start')
+            raise CancelHandler
+
+
 
     async def on_process_message(self, message: types.Message, data: dict):
-        await self.push_user_to_context(user_id=message.from_user.id, context=data)
+        await self.push_user_to_context(user_id=message.from_user.id, data=data)
 
     async def on_process_callback_query(self, callback_query: types.CallbackQuery, data: dict):
-        await self.push_user_to_context(user_id=callback_query.from_user.id, context=data)
+        await self.push_user_to_context(user_id=callback_query.from_user.id, data=data)
