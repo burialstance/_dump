c9018452f0da16fe4801fa701a8f731cd26c403f
burialstance <burialstance@gmail.com>
2021-11-02 23:13:03 +0500

    fix import

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -1,10 +1,10 @@
 from aiogram import types, Dispatcher
 
-from misc import commands
+from misc.bot_commands import BOT_COMMANDS
 
 
 async def set_default_commands(dispatcher: Dispatcher):
     await dispatcher.bot.set_my_commands([
         types.BotCommand(command, description)
-        for command, description in commands.BOT_COMMANDS.items()
+        for command, description in BOT_COMMANDS.items()
     ])
